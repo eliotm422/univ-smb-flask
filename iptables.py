b3b2b6af8468a0cff4a42577cfc18152d191719c
MUSSET Eliot <eliotmusset74@gmail.com>
2023-03-17 09:46:27 +0000

    maj

---
+++ iptables.py
@@ -0,0 +1,61 @@
+# save this as app.py
+from flask import Flask, request, url_for, redirect, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route('/home_form', methods=['GET', 'POST'])
+def home_form():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('home'))
+
+    # show the form, it wasn't submitted
+    return render_template('home.html')
+
+@app.route('/alias_form', methods=['GET', 'POST'])
+def alias_form():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('alias'))
+
+    # show the form, it wasn't submitted
+    return render_template('alias.html')
+
+@app.route('/formulaire_nat_form', methods=['GET', 'POST'])
+def formulaire_nat_form():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('formulaire_nat'))
+
+    # show the form, it wasn't submitted
+    return render_template('formulaire_nat.html')
+
+@app.route('/regle_nat_form', methods=['GET', 'POST'])
+def regle_nat_form():
+    if request.method == 'POST':
+        # do stuff when the form is submitted
+
+        # redirect to end the POST handling
+        # the redirect can be to the same route or somewhere else
+        return redirect(url_for('regle_nat'))
+
+    # show the form, it wasn't submitted
+    return render_template('regle_nat.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
---
+++ static/event.js
@@ -0,0 +1,17 @@
+function showTextcaption(id) {
+    id.innerHTML="D'origine latine, « alias » signifie « une autre fois », « autrement »";
+    
+}
+
+function showTextadip(id) {
+    id.innerHTML="L'adresse ip est notre adresse sur internet";
+
+}
+function showTextport(id) {
+    id.innerHTML="Le port permet de faire appel à des services différents";
+    
+}
+function showTextalias(id) {
+    id.innerHTML="L'Alias est blblalbalbalba";
+    
+}
